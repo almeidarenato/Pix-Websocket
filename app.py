6ce380990eb1e63ab4bf354d3cbf3d41ca4ca729
Renato Almeida <renatoam@ymail.com>
2024-02-28 23:39:59 -0300

    feat: add render template dynamic values

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix',methods=['POST'])
 def create_payment_pix():
@@ -37,8 +39,11 @@ def pix_confirmation():
 @app.route('/payments/pix/<int:payment_id>',methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
-    return render_template('payment.html',payment_id=payment.id,payment_value=payment.value, host ="http://127.0.0.1:5000", qrcode = payment.qr_code)
+    return render_template('payment.html',
+                           payment_id=payment.id,
+                           payment_value=payment.value, 
+                           host ="http://127.0.0.1:5000", 
+                           qrcode = payment.qr_code)
 
 if __name__ =='__main__':
-   app.run(debug=True)
-
+   socketio.run(app,debug=True)
